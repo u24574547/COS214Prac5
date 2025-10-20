5e76e11dcf185ec6b1d661d4ffcbbcb5d2733028
GC Nel <u24574547@tuks.co.za>
2025-10-20 14:26:40 +0200

    Added PlantDecorator types.

---
+++ FrostNet.h
@@ -0,0 +1,13 @@
+#ifndef COS214PRAC5_FROSTNET_H
+#define COS214PRAC5_FROSTNET_H
+#include "PlantDecorator.h"
+
+
+class FrostNet: public PlantDecorator {
+public:
+    FrostNet();
+    virtual ~FrostNet();
+};
+
+
+#endif //COS214PRAC5_FROSTNET_H
