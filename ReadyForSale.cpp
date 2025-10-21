f8f8f6d0fa941a1f62182022118c9537bf8c7137
KutlwanoTau <u24579484@tuks.co.za>
2025-10-21 19:50:27 +0200

    updated state files

--- ReadyForSale.cpp
+++ ReadyForSale.cpp
@@ -7,8 +7,5 @@ void ReadyForSaleState::nextState(Plant* plant) {
     if (plant->isSold()) {
         std::cout << "Plant sold! Transitioning to Sold" << std::endl;
         plant->setState(new SoldState());
-    } else {
-        std::cout << "Plant not sold, transitioning to Dormant" << std::endl;
-        plant->setState(new DormantState());
     }
 }
