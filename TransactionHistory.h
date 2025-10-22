1f182dcc07f43eb4725723f9655ca06c94682a47
stu-account <u246165692@tuks.co.za>
2025-10-22 18:38:28 +0200

    ..

--- TransactionHistory.h
+++ TransactionHistory.h
@@ -6,7 +6,7 @@
 #include "Iterator.h"
 #include "TransactionMemento.h"
 #include "Command.h"
-#include "Transact ionIterator.h"
+#include "TransactionIterator.h"
 #include <vector>
 
 class TransactionHistory : public Aggregate {
