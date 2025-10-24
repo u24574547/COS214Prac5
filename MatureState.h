bc7498b97ce74d17bfcaceea318f462e9e4563c0
KutlwanoTau <u24579484@tuks.co.za>
2025-10-24 12:18:45 +0200

    updated header files

--- MatureState.h
+++ MatureState.h
@@ -7,7 +7,7 @@ class MatureState : public PlantState {
 public:
     void nextState(Plant* plant) override;
     std::string getName() override ;
-    virtual void grow(Plant* plant) override;
+    void grow(Plant* plant) override;
     ~MatureState() override {}
 };
 
--- ReadyForSale.h
+++ ReadyForSale.h
@@ -7,7 +7,7 @@ class ReadyForSaleState : public PlantState {
 public:
     void nextState(Plant* plant) override;
     std::string getName() override;
-    virtual void grow(Plant* plant) override;
+    void grow(Plant* plant) override;
     ~ReadyForSaleState() override {}
 };
 
