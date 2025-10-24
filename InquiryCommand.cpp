5c999da9955ec3f8afaf7d42bc31e57079869af6
KutlwanoTau <u24579484@tuks.co.za>
2025-10-24 20:37:17 +0200

    Updated command files

--- InquiryCommand.cpp
+++ InquiryCommand.cpp
@@ -1,6 +1,21 @@
 #include "InquiryCommand.h"
+#include "Employee.h"
+#include <iostream>
+
+InquiryCommand::InquiryCommand(Customer* customer, Employee* mediator, const std::string& plantType)
+    : Command(customer), mediator(mediator), plantType(plantType) {}
 
 void InquiryCommand::execute() {
-    // Logic to handle customer inquiry
-    std::cout << "Handling inquiry..." << std::endl;
+    if (mediator!= nullptr){
+        std::cout << "Handling inquiry..." << std::endl;
+                mediator->handleInquiry(this);
+    }
+    else{
+        std::cout << "Inquiry could not be handled.\nNo mediator was set to handle it.";
+    }
+        
+    
 }
+
+std::string InquiryCommand::getType() const { return plantType; }
+
