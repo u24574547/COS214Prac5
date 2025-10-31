648abfef170c60d0a87326c9cb835135a0129ddd
u24614582 <u24614582@tuks.co.za>
2025-10-31 16:14:33 +0200

    added skeleton code for day (subject) class

--- Day.cpp
+++ Day.cpp
@@ -1,4 +1,10 @@
 
 #ifndef Day_CPP
 #define Day_CPP
+Day::Day() {}
+Day::Day(vector<Observer *>) {}
+void Day::addObserver(Observer *observer) {}
+void Day::removeObserver(Observer *observer) {}
+void Day::notify() {}
+
 #endif
--- Day.h
+++ Day.h
@@ -1,11 +1,24 @@
 #ifndef Day_H
 #define Day_H
 
+#include "Observer.h"
+
+#include <vector>
+
+using namespace std;
+
 class Day
 {
 public:
+    Day();
+    Day(vector<Observer *>);
+    void addObserver(Observer *observer);
+    void removeObserver(Observer *observer);
+    void notify();
+
 protected:
 private:
+    vector<Observer *> observers;
 };
 
 #endif
