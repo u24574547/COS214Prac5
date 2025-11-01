b13d592bc34fb81987397061d573ac47e77686e0
GC Nel <u24574547@tuks.co.za>
2025-11-01 14:10:11 +0200

    Fixed incorrect scope resolution operators in DeadState.cpp

--- DeadState.cpp
+++ DeadState.cpp
@@ -1,16 +1,16 @@
-#include "SoldState.h"
+#include "DeadState.h"
 #include "Plant.h"
 #include "BasePlant.h"
 
-void SoldState::nextState(Plant* plant) {
+void DeadState::nextState(Plant* plant) {
     std::cout << "Plant has been died.End of lifecycle." << std::endl;
     // No further state change
 }
 
-void SoldState::grow(Plant* plant) {
+void DeadState::grow(Plant* plant) {
     std::cout << "The plant has died.No growth is possible" << std::endl;
 }
 
-std::string SoldState::getName() {
+std::string DeadState::getName() {
     return "Dead State";
 }
