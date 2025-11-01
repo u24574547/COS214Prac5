28a805216c8e28d531a2f18b4ffacc5d7f601373
GC Nel <u24574547@tuks.co.za>
2025-11-01 17:55:59 +0200

    Made employee responses more professional.

--- Employee.cpp
+++ Employee.cpp
@@ -51,7 +51,7 @@ void Employee::handleRefund(Command *command)
 }
 void Employee::update()
 {
-    cout << name << " has ended the day to rest at home before immediately coming back." << endl;
+    cout << name << "'s working day is finished." << endl;
 }
 
 #endif
